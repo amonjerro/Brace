1e39bf48399c7ff8daeed3a83ced053f373dd564
amonjerro <eescudero_arens@hotmail.com>
2025-03-27 01:15:29 -0400

    Character select cursor management

--- Assets/Scripts/Menus/CharacterSelect/CharacterSelect.cs
+++ Assets/Scripts/Menus/CharacterSelect/CharacterSelect.cs
@@ -1,24 +1,63 @@
+using System.Collections.Generic;
 using InputManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GameMenus
 {
+
     public class CharacterSelect : AbsMenu
     {
-        public static Vector2 InputToVector(EInput input)
+        [SerializeField]
+        GameObject playerCursorPrefab;
+
+        [SerializeField]
+        List<Color> playerCursorColors;
+
+        [SerializeField]
+        GameObject characterPortraitContainer;
+
+
+        Dictionary<(int, int), RectTransform> characterPortraits;
+        List<RectTransform> cursors;
+
+        private void Start()
+        {
+            characterPortraits = new Dictionary<(int, int), RectTransform>();
+            cursors = new List<RectTransform>();
+        }
+
+        public static (int, int) InputToVector(EInput input)
         {
             switch (input) {
                 case EInput.Left:
-                    return new Vector2(-1, 0);
+                    return (-1, 0);
                 case EInput.Right:
-                    return new Vector2(1, 0);
+                    return (1, 0);
                 case EInput.Down:
-                    return new Vector2(0, 1);
+                    return (0, 1);
                 case EInput.Up:
-                    return new Vector2(0, -1);
+                    return (0, -1);
                 default:
-                    return Vector2.zero;
+                    return (0,0);
             }
+        }
+
+        public void UpdateCursorImageLocation(int playerId, (int, int) newLocation)
+        {
+            RectTransform portraitTransform = characterPortraits[newLocation];
+            RectTransform playerCursor = cursors[playerId];
+
+            playerCursor.localPosition = portraitTransform.localPosition;
+        }
+
+        public void SpawnCursor()
+        {
+            GameObject cursor = Instantiate(characterPortraitContainer);
+            cursors.Add(cursor.GetComponent<RectTransform>());
+            cursor.GetComponent<Image>().color = playerCursorColors[cursors.Count-1];
+            UpdateCursorImageLocation(cursors.Count-1, (cursors.Count - 1, 0));
+
         }
     }
 }
