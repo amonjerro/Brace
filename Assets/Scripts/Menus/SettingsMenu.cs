dc801d535b0331de8fe74e3c3925369ac96d4dfc
Eduardo Escudero <eescudero_arens@hotmail.com>
2025-05-20 19:40:05 -0400

    Volume Mixer wired to settings

--- Assets/Scripts/Menus/SettingsMenu.cs
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -1,7 +1,16 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
 namespace GameMenus
 {
     public class SettingsMenu : AbsMenu
     {
+        [SerializeField]
+        AudioMixer audioMixer;
 
+        public void SetVolume(float volume)
+        {
+            audioMixer.SetFloat("Volume", volume);
+        }
     }
 }
